c4303b4b0ef3f38594c06fccd58a5e8a40be5561
samsonsilas <samsonsilas781@gmail.com>
2023-01-08 11:12:48 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,91 @@
+#! /usr/bin/env node
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+
+
+let Convertion = {
+    "PKR": {
+      "USD": 0.0044,
+      "GBP": 0.0037,
+      "PKR": 1
+    },
+    "GBP": {
+      "USD": 1.21,
+      "PKR": 271.79,
+      "GBP": 1
+    },
+    "USD": {
+      "PKR": 225.50,
+      "GBP": 0.83,
+      "USD": 1
+    }
+  }
+
+interface ansType {
+    from: "PKR" | "GBP" | "USD",
+    to: "PKR" | "GBP" | "USD",
+    amount:number
+}
+
+const toDoList : string[]= [];
+
+const sleep = ()=>{
+    return new Promise((res)=>{
+        setTimeout(res,2000)
+    })
+}
+
+async function welcome() {
+    let title = await chalkAnimation.rainbow(`|||     CURRENCY CONVERTER    |||`);
+     await sleep();
+     title.stop()
+   
+}
+
+await welcome()
+
+async function questions(){
+  
+
+const answers : ansType = await inquirer.prompt([
+     {
+        type:'list',
+        name:'from',
+        choices:["PKR","GBP","USD"],
+        message:'From',
+        
+    },
+         {
+        type:'list',
+        name:'to',
+        choices:["PKR","GBP","USD"],
+        message:'To',
+        
+    },
+      
+
+      {
+        type:'number',
+        name:'amount',
+        message:'enter your amount',
+    
+        
+    }
+])
+
+const {from, to, amount} = answers;
+
+if(from && to && amount) {
+  let result = Convertion[from][to] * amount;
+  console.log(`Your convertion from ${from} to ${to} is ${result}`)
+} else {
+  console.log("Invalid inputs")
+}
+
+}
+
+
+questions();
+
